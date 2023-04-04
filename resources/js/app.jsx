b949fe35a60d7cc21e1551780b75b509a32d555c
sakamotosora0116 <soras.k.m.tj16@gmail.com>
2023-04-04 17:43:45 -0500

    setting SPA environment

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,9 +1,3 @@
 import './bootstrap';
-
-import ReactDOM from 'react-dom/client';
-
-import Home from './Home';
-
-ReactDOM.createRoot(document.getElementById('app')).render(
-    <Home />
-);
+import '../css/app.css';
+import './route';
